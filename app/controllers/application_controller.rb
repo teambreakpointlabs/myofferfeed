f6a29bc94677ecb90b3474dd6a5aff16befb80d8
James Haville <james.haville@rulefinancial.com>
2013-07-09 14:05:24 +0100

    removed support for ie 9 and below

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :check_browser
+
+  private
+    def check_browser
+      user_agent = UserAgent.parse(request.user_agent)
+      if user_agent.browser == 'Internet Explorer' && user_agent.version < '10.0'
+      	 render text: 'Your browser is not supported! Please upgrade your browser to view our website!'
+      end
+    end
 end
