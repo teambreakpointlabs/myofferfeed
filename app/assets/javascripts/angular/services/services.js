d42f79757bd82c57913baab31cd71d5e8c2888fd
James Haville <james.haville@rulefinancial.com>
2013-10-06 14:57:36 +0100

    trying again

--- app/assets/javascripts/angular/services/services.js
+++ app/assets/javascripts/angular/services/services.js
@@ -156,6 +156,7 @@ return {
 	var applyAffiliateLinks = function(){
 		console.log('applying skimlinks script');
 		$.ajax({
+		  type: "GET",
  		  url: "http://s.skimresources.com/js/54354X1347041.skimlinks.js",
  		  dataType: "script"
 		});
