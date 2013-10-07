5da4dbae09c73fd249ae56d304ff3c076e6b9d39
James Haville <james.haville@rulefinancial.com>
2013-10-07 12:59:43 +0100

    back

--- app/assets/javascripts/angular/services/services.js
+++ app/assets/javascripts/angular/services/services.js
@@ -155,12 +155,10 @@ return {
 	//execute skimlinks function on links appended to DOM
 	var applyAffiliateLinks = function(){
 		console.log('applying skimlinks script');
-		$( document ).ready(function() {
-	  		$.ajax({
-	 		  url: 'http://s.skimresources.com/js/54354X1347041.skimlinks.js',
-	 		  dataType: 'script'
-			});
-  		});
+		$.ajax({
+ 		  url: 'http://s.skimresources.com/js/54354X1347041.skimlinks.js',
+ 		  dataType: 'script'
+		});
 	};
 	return{
 		applyAffiliateLinks: applyAffiliateLinks
